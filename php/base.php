1d174c87ef851e4c0fdb8b216794fd72dc3f1c89
Taglialegna <alexsperanza@live.it>
2019-12-13 17:16:34 +0100

    PHP: changes login button in login link; adds comment with jQuery source.

--- php/base.php
+++ php/base.php
@@ -3,6 +3,12 @@
   <head>
     <title>Titolo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--
+    <script
+			  src="https://code.jquery.com/jquery-3.4.1.min.js"
+			  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+			  crossorigin="anonymous"></script>
+    -->
   </head>
   <body>
     <header>
@@ -10,9 +16,9 @@
         Apri menu >>
       </button>
       <img src=""/>
-      <button>
+      <a href="login.php" id="loginButton">
         Login
-      </button>
+      </a>
     </header>
     <main>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, mollitia ex eaque dolore molestias fugit quidem exercitationem, quibusdam eveniet ipsum quos adipisci. Quibusdam deserunt, quam eveniet qui perferendis quaerat non!</p>
