d1032cdfbcbca949d93c475062c191e8e21ca1fb
Alex Speranza <alexsperanza@live.it>
2019-12-13 15:25:44 +0100

    PHP: adds the template for web pages

---
+++ php/base.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <title>Titolo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+  <body>
+    <header>
+      <button>
+        Apri menu >>
+      </button>
+      <img src=""/>
+      <button>
+        Login
+      </button>
+    </header>
+    <main>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, mollitia ex eaque dolore molestias fugit quidem exercitationem, quibusdam eveniet ipsum quos adipisci. Quibusdam deserunt, quam eveniet qui perferendis quaerat non!</p>
+    </main>
+    <footer>
+      <h4>Contatti</h4>
+    </footer>
+  </body>
+</html>
