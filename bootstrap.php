7b09080052426ff9f342067cb58c2a8e68a5de0f
Ivan <ivan.perazzini@studio.unibo.it>
2020-01-28 23:19:27 +0100

    Add constants to folder paths

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
     define("TEMPLATE_DIR", "php/template/");
+    define("CSS_DIR", "css/");
+    define("IMG_DIR", "img/");
     //require_once("DatabaseHelper.php");
     //$dbh = new DatabaseHelper()
 ?>
