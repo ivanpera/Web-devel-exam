5d76df086ab66d2540dd71d3dfb8bb49ca2376b6
Ivan <ivan.perazzini@studio.unibo.it>
2020-02-02 15:47:50 +0100

    First iteration of responsive interface

--- create_event.php
+++ create_event.php
@@ -2,6 +2,6 @@
     require_once('bootstrap.php');
     $templateParams["name"] = "create_event.php";
     $templateParams["title"] = "Crea evento";
-    $templateParams["css"] = array("base.css");
+    $templateParams["css"] = array("base.css", "create_event.css");
     require(TEMPLATE_DIR."base.php");
 ?>
--- php/template/home.php
+++ php/template/home.php
@@ -1,13 +1,13 @@
 <div class="headline_text"> <!-- If we are not interested in doing something sophisticated, it might be better to remove the div -->
-    <h3> <!-- Need to test which heading works better -->
+    <h1>
         Dacci i vaini, vendiamo biglietti
-    </h3>
+    </h1>
 </div>
 <section> <!-- Is section here useful? -->
     <div class="text_hover_container">
         <a href="events.php">
             <img src="img/home_find.jpg" alt="image_find_event"/>
-            <div class="text_hover"> Cerca eventi </div> <!-- Perhaps use h4 instead of div? -->
+            <div class="text_hover"> Cerca eventi </div>
         </a>
     </div>
     <div class="text_hover_container">
