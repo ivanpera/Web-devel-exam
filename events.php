73dc968fcafe66831f36caf4580cadc55ee692d4
Taglialegna <alexsperanza@live.it>
2020-02-03 14:19:13 +0100

    PHP: events.php: adds call of popular event query

--- events.php
+++ events.php
@@ -3,5 +3,8 @@
     $templateParams["name"] = "events.php";
     $templateParams["title"] = "Visualizza eventi";
     $templateParams["css"] = array("base.css", "events.css");
+
+    $templateParams["events"] = $dbh->getPopularEvents();
+
     require(TEMPLATE_DIR."base.php");
 ?>
