8eb382c9ecefe180bdef25729be4fe58bf68a8c7
Taglialegna <alexsperanza@live.it>
2020-02-03 18:51:58 +0100

    PHP: events.php: adds template parameter for js

--- events.php
+++ events.php
@@ -3,7 +3,7 @@
     $templateParams["name"] = "events.php";
     $templateParams["title"] = "Visualizza eventi";
     $templateParams["css"] = array("base.css", "events.css");
-
+    $templateParams["js"] = array("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js", "js/events.js");
     $templateParams["events"] = $dbh->getPopularEvents();
 
     require(TEMPLATE_DIR."base.php");
