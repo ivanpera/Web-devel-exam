3ec78139895f9074dd32094c92a3f2e976662ed1
Taglialegna <alexsperanza@live.it>
2020-02-04 23:16:22 +0100

    Javascript events.js: adds addition of class 'active' to tab buttons; init active button when document is ready

--- js/events.js
+++ js/events.js
@@ -1,9 +1,21 @@
 function showTopEvents() {
     $("#top_events").css("display", "block");
     $("#search_form").css("display", "none");
+    $("#top_btn").addClass("active");
+    $("#form_btn").removeClass("active");
 }
 
 function showSearchForm() {
     $("#top_events").css("display", "none");
     $("#search_form").css("display", "block");
+    $("#form_btn").addClass("active");
+    $("#top_btn").removeClass("active");
 }
+
+$(document).ready(function() {
+    if ($("#top_events").css("display") == "block") {
+        $("#top_btn").addClass("active");
+    } else {
+        $("#form_btn").addClass("active");
+    }
+});
