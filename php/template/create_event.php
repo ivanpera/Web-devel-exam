28562867577245b51d15f9548a81ba9521a92cf1
Alex <alex.speranza@studio.unibo.it>
2020-02-05 19:26:34 +0100

    PHP: template/create_event.php: adds needed 'name' and 'required' attributes; adds button to submit the form; changes some 'name's to be an array of values; changes the input of place into a drop down list with registered places

--- php/template/create_event.php
+++ php/template/create_event.php
@@ -1,19 +1,25 @@
 <!-- Template taken from https://www.w3schools.com/howto/howto_js_form_steps.asp -->
-<form id="createForm" action="TODO">
+<form id="createForm" action="php/create_event_process.php" method="post">
     <h3>Crea un evento: </h3>
 
     <div class="tab"> Informazioni di Base:
-        <p><input type="text" placeholder="Nome dell'evento..." oninput="this.className = ''"/></p>
-        <p><input type="text" placeholder="Luogo..." oninput="this.className=''"/></p>
-        <p><input type="date" placeholder="Data..." oninput="this.className=''"/><input type="time" placeholder="Ora..." oninput="this.className=''" /></p>
+        <p><input name="nomeEvento" type="text" placeholder="Nome dell'evento..." oninput="this.className = ''" required/></p>
+        <!-- Mettere lista di luoghi disponibili con un select -->
+        <p><select name="luogo" required>
+            <option value="" disabled selected hidden>Seleziona un luogo...</option>
+            <?php foreach ($templateParams["luoghi"] as $luogo): ?>
+                <option value=<?php echo $luogo["codLuogo"];?>><?php echo $luogo["nome"]?></option>
+            <?php endforeach;?>
+        </select></p>
+        <p><input name="data" type="date" placeholder="Data..." oninput="this.className=''" required/><input name="ora" type="time" placeholder="Ora..." oninput="this.className=''" required/></p>
         <p><textarea name="description" form="createForm" placeholder="Descrizione dell'evento..." oninput="this.className = ''"></textarea></p>
     </div>
 
     <section class="tab"> Informazioni aggiuntive:
-        <p><label for="image_picker"> Scegli un'immagine: <br/><input type="file" id="image_picker"/></label></p> 
+        <p><label for="image_picker"> Scegli un'immagine: <br/><input name="" type="file" id="image_picker" name="filename"/></label></p> 
         <p>Aggiungi delle categorie: </p>
         <?php foreach ($templateParams["categories"] as $category): ?>
-            <label><input type="checkbox" name="<?php echo "category_".$category["codCategoria"];?>" value="<?php echo $category["nomeCategoria"]?>"/><?php echo $category["nomeCategoria"];?></label>
+            <label><input type="checkbox" name="categories[]" value="<?php echo $category["nomeCategoria"]?>"/><?php echo $category["nomeCategoria"];?></label>
         <?php endforeach; ?>
     </section>
 
@@ -26,8 +32,8 @@
                     <option value="<?php echo $tipoPosto["codTipologia"]; ?>"><?php echo $tipoPosto["nomeTipologia"];?></option>
                 <?php endforeach; ?>
             </select>
-            <label>Costo per biglietto (in centesimi di Euro): <input name="ticket_cost[]" type="number" min="0" step="1"/></label>
-            <label for="num_tickets"> Numero biglietti: <input type="number" min="1" name="num_tickets[]" id="num_tickets"/></label>
+            <label>Costo per biglietto (in centesimi di Euro): <input name="ticket_cost[]" type="number" min="0" step="1" required/></label>
+            <label for="num_tickets"> Numero biglietti: <input type="number" min="1" name="num_tickets[]" id="num_tickets" required/></label>
             <button class="rm_ticket_btn" type="button" onclick=removeLastTicket()> - </button> <!-- classden for the first ticket type -->
             <button class="add_ticket_btn" type="button" onclick=addNewTicket()> + </button> <!-- Juclass a placeholder: adds another ticket to edit (todo with js) -->
         </div>
@@ -40,6 +46,8 @@
             <button class="add_mod_btn" type="button" onclick=addNewMod()> + </button> <!-- Just a placeholder: adds another moderator to edit (todo with js) -->
         </div>
     </section>
+
+    <input type="submit"/>
 
     <div style="overflow:auto;">
         <div style="float:right;">
