eb0ab9b07726368ad85138c19b47ed3c940f6f0a
Alex <alex.speranza@studio.unibo.it>
2020-02-05 16:49:26 +0100

    PHP: events.php: adds previous page variable; adds control of shown events based on NSFC of the user

--- events.php
+++ events.php
@@ -1,10 +1,11 @@
 <?php
     require_once('bootstrap.php');
+    $_SESSION["previousPage"] = "events.php";
     $templateParams["name"] = "events.php";
     $templateParams["title"] = "Visualizza eventi";
     $templateParams["css"] = array("base.css", "events.css");
     $templateParams["js"] = array("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js", "js/events.js", "js/base.js");
-    $templateParams["events"] = $dbh->getPopularEvents();
+    $templateParams["events"] = $dbh->getPopularEvents(isset($_SESSION["sessUser"]["NSFC"]) ? $_SESSION["sessUser"]["NSFC"] : 0);
 
     require(TEMPLATE_DIR."base.php");
 ?>
