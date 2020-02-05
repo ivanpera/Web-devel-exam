74065a21aff7f46f7cdfb872aba005b99e5fb2ff
Alex <alex.speranza@studio.unibo.it>
2020-02-05 16:51:13 +0100

    PHP: event_details.php: adds query to get the event; adds checks of existence of the searched event and controls to check whether the user is allowed to see the event

--- event_details.php
+++ event_details.php
@@ -1,5 +1,27 @@
 <?php
     require_once('bootstrap.php');
+    if (!isset($_GET["codEvento"])){
+        header("Location: index.php");
+        die();
+    }
+    $templateParams["evento"] = $dbh->getEvent($_GET["codEvento"])[0];
+    if($templateParams["evento"] != false) {
+        if ($templateParams["evento"]["NSFC"] == "1") {
+            if(!isset($_SESSION["sessUser"])) {
+                $_SESSION["previousPage"] = "event_details.php?codEvento=".$_GET["codEvento"];
+                session_write_close();
+                header("Location: login.php");
+                die();
+            } else if ($_SESSION["sessUser"]["NSFC"] == 0) {
+                //Volendo si manda in una pagina esplicativa
+                header("Location: index.php");
+                die();
+            }
+        }
+    } else {
+        header("Location: index.php");
+        die();
+    }
     $templateParams["name"] = "event_details.php";
     $templateParams["title"] = "Dettagli evento";
     $templateParams["css"] = array("base.css");
