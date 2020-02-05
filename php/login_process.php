c018c44a0dbb9387365b02ba378012f487aa35bd
Alex <alex.speranza@studio.unibo.it>
2020-02-05 16:47:44 +0100

    PHP: login_process.php: adds new session variable to check age of the logged user; adds handling of redirection variable

--- php/login_process.php
+++ php/login_process.php
@@ -4,11 +4,13 @@
     $result = $dbh->checkLogin($_POST["email"], $_POST["password"]);
     if (!empty($result)) {
         $_SESSION["sessUser"]["email"] = $_POST["email"];
-        session_write_close();
+        $_SESSION["sessUser"]["NSFC"] = date_diff(new DateTime($result[0]["dataNascita"]), new DateTime(date("Y-m-d")), true)->y > 18 ? "1" : "0";
         $redirectPage = "index.php";
         if (isset($_SESSION["previousPage"])) {
             $redirectPage = $_SESSION["previousPage"];
+            unset($_SESSION["previousPage"]);
         }
+        session_write_close();
         header("Location: ../".$redirectPage);
         die();
     } else {
