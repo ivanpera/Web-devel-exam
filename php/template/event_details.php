0312c388d8d443491a48afd5054bad7aca7589f0
Alex <alex.speranza@studio.unibo.it>
2020-02-05 16:52:13 +0100

    PHP: template/event_details.php: fills the page with information got with the query

--- php/template/event_details.php
+++ php/template/event_details.php
@@ -1,10 +1,10 @@
 <section>
-    <img src="" alt="event_image"/>
-    <h3> Event title </h3>
+    <img src="<?php echo "img/".(file_exists($templateParams["evento"]["emailOrganizzatore"].$templateParams["evento"]["nomeImmagine"]) ? $templateParams["evento"]["emailOrganizzatore"].$templateParams["evento"]["nomeImmagine"] : "image-not-available.jpg")?>" alt="event_image"/>
+    <h3><?php echo $templateParams["evento"]["nomeEvento"]?> </h3>
+    <p><?php echo $templateParams["evento"]["dataEOra"]?>
+    <p>  <?php echo $templateParams["evento"]["descrizione"] ?> </p>
 
-    <p>  Lorem ipsum chachacha </p>
-
-    <p> Indirizzo </p>
+    <p> Dove: <?php echo $templateParams["evento"]["nomeLuogo"]." @ ".$templateParams["evento"]["indirizzo"]?> </p>
 
     <label for="btn_show_on_map">
         <button type="button" id="btn_show_on_map">
@@ -12,8 +12,11 @@
         </button>
     </label>
 
+    <p>
+        Biglietti: <?php echo $templateParams["evento"]["postiOccupati"]." occupati su ".$templateParams["evento"]["capienzaMassima"]?>
+    </p>
+    <p> Organizzatore: <?php echo $templateParams["evento"]["emailOrganizzatore"] ?> </p>
+    <p> Moderatori: </p>
+    
     <!--php check, if the the user is not logged in, display the "log in first to purchase", else display the "proceed to check out" -> disable it if no tickets are selected -->
-
-    Biglietti disponibili
-
 </section>
