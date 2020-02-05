ce290418489cf1e27268c9a7557ca48af83ac44a
Alex <alex.speranza@studio.unibo.it>
2020-02-05 12:42:03 +0100

    PHP: create_events.php: adds mechanism to prevent access to the page when the user is not loggen in; adds function calls to the new queries

--- create_event.php
+++ create_event.php
@@ -1,8 +1,17 @@
 <?php
     require_once('bootstrap.php');
+    //This check is present to prevent accessing the page without being logged in and by changing the browser address
+    if(!isset($_SESSION["sessUser"])) {
+        $_SESSION["previousPage"] = "create_event.php";
+        session_write_close();
+        header("Location: login.php");
+        die();
+    }
     $templateParams["name"] = "create_event.php";
     $templateParams["title"] = "Crea evento";
     $templateParams["css"] = array("base.css", "create_event.css");
-    $templateParams["js"] = array("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js", "js/base.js");
+    $templateParams["js"] = array("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js", "js/base.js", "js/create_event.js");
+    $templateParams["categories"] = $dbh->getCategories();
+    $templateParams["tipoPosti"] = $dbh->getSeatTypes();
     require(TEMPLATE_DIR."base.php");
 ?>
