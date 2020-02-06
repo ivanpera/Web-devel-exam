ac07717c0026852a77ff85c65e267dd3d99ed5d9
Alex <alex.speranza@studio.unibo.it>
2020-02-06 17:19:46 +0100

    PHP: event_details.php: changes every segment of header() calls to safeHeader() calls; changes check to the passed codEvento to include the case where the codEvento point to an inexistant event; adds a parameter with email of the event moderators

--- event_details.php
+++ event_details.php
@@ -1,27 +1,25 @@
 <?php
     require_once('bootstrap.php');
     if (!isset($_GET["codEvento"])){
-        header("Location: index.php");
-        die();
+        safeHeader("Location: index.php");
     }
+
+
     $templateParams["evento"] = $dbh->getEvent($_GET["codEvento"])[0];
-    if($templateParams["evento"] != false) {
+    if(!empty($templateParams["evento"])) {
         if ($templateParams["evento"]["NSFC"] == "1") {
             if(!isset($_SESSION["sessUser"])) {
                 $_SESSION["previousPage"] = "event_details.php?codEvento=".$_GET["codEvento"];
-                session_write_close();
-                header("Location: login.php");
-                die();
+                safeHeader("Location: login.php");
             } else if ($_SESSION["sessUser"]["NSFC"] == 0) {
                 //Volendo si manda in una pagina esplicativa
-                header("Location: index.php");
-                die();
+                safeHeader("Location: index.php");
             }
         }
     } else {
-        header("Location: index.php");
-        die();
+        safeHeader("Location: index.php");
     }
+    $templateParams["moderatori"] = $dbh->getEventModerators($_GET["codEvento"]);
     $templateParams["name"] = "event_details.php";
     $templateParams["title"] = "Dettagli evento";
     $templateParams["css"] = array("base.css");
