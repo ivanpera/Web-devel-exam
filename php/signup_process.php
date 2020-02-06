e9b733fa1d8ca40779d4ecd9c96150fa3348edaf
Alex <alex.speranza@studio.unibo.it>
2020-02-06 17:41:44 +0100

    PHP: signup_process.php: adds redirection on successfull registration; adds comment to a possible alternative solution to the previous mentioned redirection

--- php/signup_process.php
+++ php/signup_process.php
@@ -1,9 +1,10 @@
 <?php
     require_once('bootstrap.php');
     if($dbh->registerNewUser($_POST["email"], $_POST["password"], $_POST["name"], $_POST["surname"], $_POST["birthdate"], $_POST["gender"], 0)) {
-        header("Location: ../signup.php?registrationFailed=1");
+        safeHeader("Location: ../signup.php?registrationFailed=1");
     } else {
-        echo "Registrazione corretta";
+        //Volendo si può già settare la sessione con le variabili dell'utente e loggarlo automaticamente, poi ridirezionarlo alla home
+        safeHeader("Location: ../login.php");
     }
 
 ?>
