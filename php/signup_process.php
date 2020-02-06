fe8092a7da3b9acb9f17ee1464f61b3ff28cd566
Alex <alex.speranza@studio.unibo.it>
2020-02-06 17:24:17 +0100

    PHP: php/signup_process.php: changes requirement from 'DatabaseHelper.php' into 'bootstrap.php'

--- php/signup_process.php
+++ php/signup_process.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('DatabaseHelper.php');
+    require_once('bootstrap.php');
     if($dbh->registerNewUser($_POST["email"], $_POST["password"], $_POST["name"], $_POST["surname"], $_POST["birthdate"], $_POST["gender"], 0)) {
         header("Location: ../signup.php?registrationFailed=1");
     } else {
