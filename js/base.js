30821991a5af754fc4029f27a004884bdbe40653
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-02-07 15:56:42 +0100

    base.js: make sidebar's animation responsive and add fade transition to the overlay

--- js/base.js
+++ js/base.js
@@ -1,12 +1,19 @@
-function openSidebar() {
-    $("#sidebar").css("display", "block"); 
-    $("#sidebar").animate({left: '0'});
+const sidebarAnimDuration = 180;
 
+function openSidebar() {
+    $("#sidebar").css("display", "block");
+    $("#sidebar").css("left", -$("#sidebar").width());
+    $("#sidebar").animate({left: '0'}, sidebarAnimDuration);
+    $("#overlay").css("display", "block");
+    $("#overlay").animate({opacity: '0.5'}, sidebarAnimDuration);
 }
 
+
 function closeSidebar() {
-    $("#sidebar").animate({left: '-80%'}, function() {
+    $("#overlay").animate({opacity: '0'}, sidebarAnimDuration);
+    $("#sidebar").animate({left: -$("#sidebar").width()}, sidebarAnimDuration, function() {
         // The change in display property is inside a callback function so it is done after the completion of the translation.
         $("#sidebar").css("display", "none");
+        $("#overlay").css("display", "none");
     });
 }
