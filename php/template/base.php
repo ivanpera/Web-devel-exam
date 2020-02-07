728ba3588178210fb64d3be44cc7d030530388d3
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-02-07 16:14:43 +0100

    base.php: clear document, add logo to sidebar

--- php/template/base.php
+++ php/template/base.php
@@ -28,17 +28,9 @@
           endforeach;
       endif;
     ?>
-    <!--
-    <script
-			  src="https://code.jquery.com/jquery-3.4.1.min.js"
-			  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-			  crossorigin="anonymous"></script>
-    -->
   </head>
   <body>
-    <header>  <!-- Perhaps should we use a nav?-->
-      <!-- NOTE: currently, in the mockup, the sidebar button and login icon
-          are not present in the login page, should we make a special case?-->
+    <header>
         <?php 
         if (!isset($_SESSION["sessUser"]["email"])) { 
           echo '<a href="login.php">
@@ -58,12 +50,14 @@
       ?>
     </main>
 
-<!-- Maybe add an overlay which blocks input on the page below the 
-     aside and which appears and disappears with the aside        -->
     <aside id=sidebar>
-        <button id="closeside_btn" onclick=closeSidebar() type="button">
-          <img src="img/closeSidebarIcon.png" alt="Close sidebar"/>
-        </button>
+        <header>
+          <button id="closeside_btn" onclick=closeSidebar() type="button">
+            <img src="img/openSidebarIcon.png" alt="Close sidebar"/>
+          </button><a href="index.php">
+            <img src="img/logo.png" alt="home" class="logo"/>
+          </a>
+      </header>
         <ul>
           <li>Profilo</li>
           <li>Calendario</li>
@@ -73,28 +67,7 @@
           <li><a href="php/logout_process.php">Logout</a></li>
         </ul>
     </aside>
-    <!-- <aside id="sidebar">
-          These elements are identical to the ones used in the header section, might be better to generate them through js?
-        <button id="btn_close_sidebar" type="button">
-          <img src="" alt="icon_sidebar"/>
-        </button>
-        <a href="index.php">
-          <img src="" alt="home"/>
-        </a>
-        <ul>
-          To be filled through js depending on the type of user
-              Possible list items:
-                -profilo
-                -calendario
-                -statistiche
-                -messaggi (promemoria/avvisi)
-                -gestione eventi (moderazione)
-                -???
-        </ul>
-        <a href="login.php">
-          <img src="" alt="login"/>
-        </a>
-    </aside> -->
+    <div id="overlay"></div>
 
     <footer>
       Contatti
