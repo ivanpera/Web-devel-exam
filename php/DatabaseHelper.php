46710ef7c248ad36d79394f37cb480e9c777546d
Alex Speranza <alexsperanza@live.it>
2020-02-08 14:43:01 +0100

    PHP: DatabaseHelper.php: fixes constructor

--- php/DatabaseHelper.php
+++ php/DatabaseHelper.php
@@ -6,7 +6,7 @@ class DatabaseHelper{
     public function __construct($servername, $username, $password, $dbname){
         $this->db = new mysqli($servername, $username, $password, $dbname);
         if ($this->db->connect_error) {
-            die("Connection failed: " . $db->connect_error);
+            die("Connection failed: " . $this->db->connect_error);
         }        
     }
 
