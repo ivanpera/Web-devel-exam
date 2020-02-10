0f0c2d8366f27b3b1a5ac8efae4eef5b5bb939dc
Alex Speranza <alexsperanza@live.it>
2020-02-10 16:50:40 +0100

    PHP: modify_event_process: adds cycle to compact ticket values; adds call to updateEvent function

--- php/modify_event_process.php
+++ php/modify_event_process.php
@@ -5,9 +5,6 @@
     }
     define("ONE_MiB", 1048576);
 
-    var_dump($_POST);
-    var_dump($_FILES);
-
     if(!empty($_FILES["imageName"]["name"])) {
         $imageName = uploadImage();
         if(!empty($_POST["currentImageName"])) {
@@ -24,13 +21,23 @@
     $NSFC = isset($_POST["NSFC"]) ? 1 : 0;
     $categories = $_POST["categories"];
     $emailModeratori = $_POST["mod_mail"];
-    //Update evento, categorie, moderatori
 
     $biglietti["type"] = $_POST["ticket_type"];
     $biglietti["cost"] = $_POST["ticket_cost"];
     foreach ($biglietti["cost"] as &$costo) {$costo = intval($costo) * 100;}
     $biglietti["num"] = $_POST["num_tickets"];
 
-    //safeHeader("Location: ../event_details.php?codEvento=".$codEvento);
+    //Mette assieme le categorie con i costi uguali
+    for ($i=0; $i < count($biglietti["type"]); $i++) { 
+        $prevOccurrence = array_search($biglietti["type"][$i], $biglietti["type"]);
+        if ($prevOccurrence !== false && $prevOccurrence < $i && $biglietti["cost"][$i] == $biglietti["cost"][$prevOccurrence]) {
+            $biglietti["num"][$prevOccurrence] += $biglietti["num"][$i];
+            unset($biglietti["type"][$i], $biglietti["cost"][$i], $biglietti["num"][$i]);
+            $i--;
+        }
+    }
+
+    $dbh->updateEvent($codEvento, $eventName, $dataEOra, $NSFC, $descrizione, $imageName, $codLuogo, $categories, $biglietti, $emailModeratori);
+    safeHeader("Location: ../event_details.php?codEvento=".$codEvento);
     
 ?>
