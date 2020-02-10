abd61d5a2489578f42fb851f8b150381bef19ee3
Alex Speranza <alexsperanza@live.it>
2020-02-10 10:40:35 +0100

    Database: moves sql statements to comply to key dependencies

--- db/StubData.sql
+++ db/StubData.sql
@@ -1,4 +1,33 @@
 --
+-- Dump dei dati per la tabella `tipologia_posto`
+--
+
+INSERT INTO `tipologia_posto` (`codTipologia`, `nomeTipologia`) VALUES
+(1, 'Per terra');
+
+--
+-- Dump dei dati per la tabella `luogo`
+--
+
+INSERT INTO `luogo` (`codLuogo`, `nome`, `indirizzo`, `urlMaps`, `capienzaMassima`) VALUES
+(1, 'Arena di Verona', 'Via dell\'arena, 1', '', 11),
+(2, 'Colosseo', 'Via del colosseo, 1', '', 11);
+
+--
+-- Dump dei dati per la tabella `utente`
+--
+
+INSERT INTO `utente` (`email`, `userPassword`, `nome`, `cognome`, `dataNascita`, `genere`, `dataIscrizione`, `organizzatore`, `amministratore`) VALUES
+('alexsperanza@live.it', '434ea961e2cb040cdbc2288467da95a2358a4c9adb32c70f3e4296d6f1859c6054baff94f293dc35c974a77f53caca32d4a15662bf0cec858e6b719fd208a87d', 'Alex', 'Speranza', '1998-07-06', 'M', '2020-01-29', b'0', b'1');
+
+--
+-- Dump dei dati per la tabella `prenotazione`
+--
+
+INSERT INTO `prenotazione` (`codPrenotazione`, `dataEOra`, `costoTotale`, `differenzaGiorni`, `emailUtente`) VALUES
+(1, '2020-01-30 15:24:00', 35, 1, 'alexsperanza@live.it');
+
+--
 -- Dump dei dati per la tabella `categoria_evento`
 --
 
@@ -28,13 +57,6 @@ INSERT INTO `evento_ha_categoria` (`codCategoria`, `codEvento`) VALUES
 (3, 1),
 (1, 2);
 
---
--- Dump dei dati per la tabella `luogo`
---
-
-INSERT INTO `luogo` (`codLuogo`, `nome`, `indirizzo`, `urlMaps`, `capienzaMassima`) VALUES
-(1, 'Arena di Verona', "Via dell'arena", '1', '', 11),
-(2, 'Colosseo', 'Via del colosseo, 1', '', 11);
 
 
 --
@@ -64,25 +86,3 @@ INSERT INTO `posto` (`codEvento`, `codPosto`, `costo`, `codTipologia`, `codPreno
 (2, '9', 3, 1, NULL);
 
 
---
--- Dump dei dati per la tabella `prenotazione`
---
-
-INSERT INTO `prenotazione` (`codPrenotazione`, `dataEOra`, `costoTotale`, `differenzaGiorni`, `emailUtente`) VALUES
-(1, '2020-01-30 15:24:00', 35, 1, 'alexsperanza@live.it');
-
-
---
--- Dump dei dati per la tabella `tipologia_posto`
---
-
-INSERT INTO `tipologia_posto` (`codTipologia`, `nomeTipologia`) VALUES
-(1, 'Per terra');
-
-
---
--- Dump dei dati per la tabella `utente`
---
-
-INSERT INTO `utente` (`email`, `userPassword`, `nome`, `cognome`, `dataNascita`, `genere`, `dataIscrizione`, `organizzatore`, `amministratore`) VALUES
-('alexsperanza@live.it', '434ea961e2cb040cdbc2288467da95a2358a4c9adb32c70f3e4296d6f1859c6054baff94f293dc35c974a77f53caca32d4a15662bf0cec858e6b719fd208a87d', 'Alex', 'Speranza', '1998-07-06', 'M', '2020-01-29', b'0', b'0');
