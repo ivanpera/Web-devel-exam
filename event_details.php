6dcaeac37222600c445739fc8d492e43fbcbf70f
Alex Speranza <alexsperanza@live.it>
2020-02-11 13:16:00 +0100

    PHP: event_details.php: adds inclusion of its javascript file

--- event_details.php
+++ event_details.php
@@ -23,6 +23,6 @@
     $templateParams["name"] = "event_details.php";
     $templateParams["title"] = "Dettagli evento";
     $templateParams["css"] = array("base.css");
-    $templateParams["js"] = array("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js", "js/base.js");
+    $templateParams["js"] = array("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js", "js/base.js", "js/event_details.js");
     require(TEMPLATE_DIR."base.php");
 ?>
