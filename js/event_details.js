22e97779b093a2ebfbc80a54e732272cbfe7c253
Alex Speranza <alexsperanza@live.it>
2020-02-12 17:24:33 +0100

    Javascript: event_details: adds function to send an AJAX request to the server to add seats into the cart

--- js/event_details.js
+++ js/event_details.js
@@ -32,3 +32,17 @@ function updateButtonText(codEvento, email) {
     xhttp.open("GET", "php/ajax_response/observe_event_status.php?codEvento=" + codEvento + "&emailUtente=" + email, true);
     xhttp.send();
 }
+
+function addToCart(codEvento, codTipologia, costo) {
+  let numOfTickets = $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prev().val();
+
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if(this.readyState == 4 && this.status == 200) {
+      $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().html((xhttp.response == "OK" ? "Aggiunto al carrello!" : "Errore!"));
+    }
+  };
+  xhttp.open("POST", "php/ajax_response/cart_handler.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("codEvento="+codEvento+"&codTipologia="+codTipologia+"&costo="+costo+"&num="+numOfTickets);
+}
