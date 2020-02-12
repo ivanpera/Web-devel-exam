5e21786aeca9ff0722c53ea1aa445ebd6c6facf7
Alex Speranza <alexsperanza@live.it>
2020-02-12 19:00:37 +0100

    PHP: ajax_response/cart_handler: changes index of session variable to store ticket in the cart

--- php/ajax_response/cart_handler.php
+++ php/ajax_response/cart_handler.php
@@ -1,10 +1,10 @@
 <?php
   require_once('../../bootstrap.php');
   if (isset($_SESSION["sessUser"]) && !empty($_POST)) {
-    if (!isset($_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]][$_POST["costo"]])) {
-      $_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]][$_POST["costo"]] = 0;
+    if (!isset($_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]."/".$_POST["costo"]])) {
+      $_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]."/".$_POST["costo"]] = 0;
     }
-    $_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]][$_POST["costo"]] += $_POST["num"];
+    $_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]."/".$_POST["costo"]] += $_POST["num"];
     echo "OK";
   } else {
     safeHeader("Location: ../../index.php");
