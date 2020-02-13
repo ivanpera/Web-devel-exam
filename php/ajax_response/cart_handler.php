dd77d843935f472ebee54cd9821a233224adc692
Alex Speranza <alexsperanza@live.it>
2020-02-13 09:57:33 +0100

    PHP: ajax_response/cart_handler: adds check to remove empty variables of the cart

--- php/ajax_response/cart_handler.php
+++ php/ajax_response/cart_handler.php
@@ -5,6 +5,12 @@
       $_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]."/".$_POST["costo"]] = 0;
     }
     $_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]."/".$_POST["costo"]] += $_POST["num"];
+    if($_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]."/".$_POST["costo"]] <= 0) {
+      unset($_SESSION["sessUser"]["cart"][$_POST["codEvento"]][$_POST["codTipologia"]."/".$_POST["costo"]]);
+    }
+    if(empty($_SESSION["sessUser"]["cart"][$_POST["codEvento"]])) {
+      unset($_SESSION["sessUser"]["cart"][$_POST["codEvento"]]);
+    }
     echo "OK";
   } else {
     safeHeader("Location: ../../index.php");
