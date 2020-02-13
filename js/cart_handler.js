ddc31623bd3f73930c5c3391641c5aa611c26a2f
Alex Speranza <alexsperanza@live.it>
2020-02-13 09:55:45 +0100

    Javascript: cart_handler: moves function to handle cart into a new file; adds 2 function to remove one item or all the items from the cart

---
+++ js/cart_handler.js
@@ -0,0 +1,31 @@
+function addToCart(codEvento, codTipologia, costo) {
+  let numOfTickets = $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prev().val();
+
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if(this.readyState == 4 && this.status == 200) {
+      $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().html((xhttp.response == "OK" ? "Aggiunto al carrello!" : "Errore!"));
+      $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prop("disabled", true);
+    }
+  };
+  xhttp.open("POST", "php/ajax_response/cart_handler.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("codEvento="+codEvento+"&codTipologia="+codTipologia+"&costo="+costo+"&num="+numOfTickets);
+}
+
+function removeFromCart(codEvento, codTipologia, costo, num) {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    console.log(xhttp);
+    if(this.readyState == 4 && this.status == 200) {
+      location.reload(true);
+    }
+  };
+  xhttp.open("POST", "php/ajax_response/cart_handler.php");
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("codEvento="+codEvento+"&codTipologia="+codTipologia+"&costo="+costo+"&num=-"+num);
+}
+
+function emptyCart() {
+  $("button.cartRemover").each(function() {$(this).click();});
+}
--- js/event_details.js
+++ js/event_details.js
@@ -32,18 +32,3 @@ function updateButtonText(codEvento, email) {
     xhttp.open("GET", "php/ajax_response/observe_event_status.php?codEvento=" + codEvento + "&emailUtente=" + email, true);
     xhttp.send();
 }
-
-function addToCart(codEvento, codTipologia, costo) {
-  let numOfTickets = $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prev().val();
-
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if(this.readyState == 4 && this.status == 200) {
-      $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().html((xhttp.response == "OK" ? "Aggiunto al carrello!" : "Errore!"));
-      $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prop("disabled", true);
-    }
-  };
-  xhttp.open("POST", "php/ajax_response/cart_handler.php", true);
-  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("codEvento="+codEvento+"&codTipologia="+codTipologia+"&costo="+costo+"&num="+numOfTickets);
-}
