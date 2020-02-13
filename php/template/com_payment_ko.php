0a3780be590ce1ed2427bdc90c2dc8a69de5fd75
Alex Speranza <alexsperanza@live.it>
2020-02-13 11:46:54 +0100

    PHP: adds page to display communications and 2 communication regarding payment success

---
+++ php/template/com_payment_ko.php
@@ -0,0 +1,1 @@
+<p>Il pagamento ha riscontrato errori. Possibili errori possono essere un credito residuo insufficiente o un numero di biglietti scelti maggiore del numero di biglietti disponibili. Torna alla <a href="index.php">homepage</a> o al <a href="shopping_cart.php">carrello</a>.</p>
---
+++ php/template/com_payment_ok.php
@@ -0,0 +1,1 @@
+<p>Il pagamento è avvenuto con successo! Torna alla <a href="index.php">homepage</a> oppure vai al <a href="user_area.php">tuo profilo</a>.</p>
