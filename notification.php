079d100b508acc243ae0975371ddda2376557a65
Alex Speranza <alexsperanza@live.it>
2020-02-13 17:05:35 +0100

    PHP, JS: notification: adds page to display a preview of the user notification

---
+++ notification.php
@@ -0,0 +1,17 @@
+<?php
+  require_once("bootstrap.php");
+  if(!isset($_SESSION["sessUser"])) {
+    $_SESSION["previousPage"] = "notification.php";
+    safeHeader("Location: login.php");
+  }
+  $userEmail = $_SESSION["sessUser"]["email"];
+
+  $templateParams["name"] = "notification.php";
+  $templateParams["title"] = "Notifiche";
+  $templateParams["css"] = array("base.css");
+  $templateParams["js"] = array("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js", "js/base.js", "js/notification.js");
+
+  $templateParams["notifiche"] = $dbh->getNotificationFor($userEmail);
+
+  require(TEMPLATE_DIR."base.php");
+?>
