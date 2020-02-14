715ad41800ea89f46083381d9003a66413e8af13
Alex Speranza <alexsperanza@live.it>
2020-02-14 09:42:38 +0100

    PHP: template/notification.php: fixes the link to a specific notification (it was using the event id as the notification id)

--- php/template/notification.php
+++ php/template/notification.php
@@ -1,7 +1,7 @@
 <?php if (count($templateParams["notifiche"]) > 0) :?>
   <button onclick="readAll()">Segna tutti come letto</button>
   <?php foreach($templateParams["notifiche"] as $notifica):?>
-    <a href="notification_details.php?codEvento=<?php echo $notifica["codEvento"]?>&codNotifica=<?php echo $notifica["codEvento"]?>">
+    <a href="notification_details.php?codEvento=<?php echo $notifica["codEvento"]?>&codNotifica=<?php echo $notifica["codNotifica"]?>">
       <article class="notification<?php echo $notifica["letta"] == false ? "unread" : "";?>" >
         <h3><?php echo $notifica["titolo"];?></h3>
         <p>Inviata alle <?php echo $notifica["dataEOraInvio"];?></p>
