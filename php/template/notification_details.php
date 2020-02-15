11dd2d357f1df9f910c115cd76fccdd76f6895ce
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-02-16 00:37:50 +0100

    template/notification_details: add main-content div

--- php/template/notification_details.php
+++ php/template/notification_details.php
@@ -1,6 +1,8 @@
-<section>
-  <h3><?php echo $templateParams["notifica"]["titolo"]?></h3>
-  <p>Data e ora invio: <?php echo $templateParams["notifica"]["dataEOraInvio"];?></p>
-  <p><?php echo $templateParams["notifica"]["descrizione"]?></p>
-  <a href="event_details.php?codEvento=<?php echo $templateParams["notifica"]["codEvento"]?>">Mostra evento</a>
-</section>
+<div class="main-content">
+  <section>
+    <h3><?php echo $templateParams["notifica"]["titolo"]?></h3>
+    <p>Data e ora invio: <?php echo $templateParams["notifica"]["dataEOraInvio"];?></p>
+    <p><?php echo $templateParams["notifica"]["descrizione"]?></p>
+    <a href="event_details.php?codEvento=<?php echo $templateParams["notifica"]["codEvento"]?>">Mostra evento</a>
+  </section>
+</div>
