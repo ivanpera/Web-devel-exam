ff2d11928f4f3666b1a5fa86c33ef8e56ced602f
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-02-16 00:35:56 +0100

    template/event_review: add use of list-item class

--- php/template/event_reviews.php
+++ php/template/event_reviews.php
@@ -3,7 +3,7 @@
     <h3>Recensioni per l'evento <?php echo $templateParams["nomeEvento"]?></h3>
     <p>Recensioni: <a href="event_reviews.php?codEvento=<?php echo $_GET["codEvento"]?>">(Mostra tutte le recensioni)</a></p>
       <?php foreach($templateParams["recensioni"] as $recensione ): ?>
-        <article>
+        <article class="list-item">
           <h5><?php echo ($recensione["anonima"] == 0 ? $recensione["emailUtente"] : "Anonimo")?></h5>
           <p>Voto: <?php echo $recensione["voto"]?></p>
           <p><?php echo $recensione["testo"]?></p>
