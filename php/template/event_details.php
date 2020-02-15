6f882b2676cd2e5ae91290b79b661c1ff3c62edd
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-02-16 00:34:17 +0100

    template/event_details: improve html content, add use of list-item class

--- php/template/event_details.php
+++ php/template/event_details.php
@@ -62,11 +62,11 @@
             <a href="create_review.php?codEvento=<?php echo $_GET["codEvento"]?>">Recensisci questo evento</a>
         <?php endif;?>
         <?php if(!empty($templateParams["recensioni"])):?>
-            <p>Recensioni: <a href="event_reviews.php?codEvento=<?php echo $_GET["codEvento"]?>">(Mostra tutte le recensioni)</a></p>
+            <p>Recensioni più recenti: <a href="event_reviews.php?codEvento=<?php echo $_GET["codEvento"]?>">(Mostra tutte le recensioni)</a></p>
             <p>Voto medio: <?php printf("%.1f",$templateParams["votoMedioRecensioni"])?></p>
             <?php foreach($templateParams["recensioni"] as $recensione ): ?>
-                <article>
-                    <h5><?php echo ($recensione["anonima"] == 0 ? $recensione["emailUtente"] : "Anonimo")?></h5>
+                <article class="list-item">
+                    <h5>Recensione di: <?php echo ($recensione["anonima"] == 0 ? $recensione["emailUtente"] : "Anonimo")?></h5>
                     <p>Voto: <?php echo $recensione["voto"]?></p>
                     <p><?php echo $recensione["testo"]?></p>
                     <p>Scritta il: <?php echo $recensione["dataScrittura"]?></p>
