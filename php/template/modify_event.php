abb25f45241a9e829ef56e70313f98181b6f0bd4
Alex Speranza <alexsperanza@live.it>
2020-02-15 14:24:24 +0100

    PHP: template/modify_event.php: adds hidden input which send the current image name

--- php/template/modify_event.php
+++ php/template/modify_event.php
@@ -18,6 +18,7 @@
 
         <section class="tab">
             <h4>Informazioni aggiuntive:</h4>
+            <input type="text" value="<?php echo $templateParams["evento"]["nomeImmagine"]?>" name="currentImageName" hidden/>
             <label for="image_picker"> Scegli un'immagine: <br/><input type="file" id="image_picker" name="imageName"/></label>
             <label for="catg_list">Aggiungi delle categorie:<ul id="catg_list">
             <?php foreach ($templateParams["categories"] as $category): ?>
