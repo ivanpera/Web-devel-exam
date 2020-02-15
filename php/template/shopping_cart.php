c60c6daa3e8bb4004401cd7412c003258cc1c67e
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-02-16 00:42:37 +0100

    template/shopping_cart: add main-content div, add use of headings and list-item and list-container classes

--- php/template/shopping_cart.php
+++ php/template/shopping_cart.php
@@ -1,22 +1,27 @@
-<?php if (!empty($templateParams["eventi"])) : ?>
-  <button type="button" class="cartEmptier" onclick="emptyCart()">Svuota carrello</button>
-  <?php foreach ($templateParams["eventi"] as $evento): ?>
-    <div class="evento">
-      <p><?php echo $evento["nomeEvento"]?>
-      <p><?php echo $evento["dataEOra"]?></p>
-      <p><?php echo $evento["nomeLuogo"]." @ ".$evento["indirizzo"];?></p>
-      <?php foreach ($_SESSION["sessUser"]["cart"][$evento["codEvento"]] as $tipoCosto => $numero): ?>
-        <div class="postiEvento">
-          <?php $arrTipoCosto = explode("/",$tipoCosto); ?>
-          <p>Tipologia: <?php echo $dbh->getSeatType($arrTipoCosto[0])["nomeTipologia"]; ?></p>
-          <p><?php echo (intval($arrTipoCosto[1])/100)."€ * ".$numero." = ".(intval($arrTipoCosto[1]) / 100 * intval($numero))."€";?></p> 
-          <button type="button" class="cartRemover" onclick='removeFromCart(<?php echo $evento["codEvento"].",".$arrTipoCosto[0].",".$arrTipoCosto[1].",".$numero;?>)'>Rimuovi voce</button>
+<div class="main-content">
+  <?php if (!empty($templateParams["eventi"])) : ?>
+    <div>
+      <?php foreach ($templateParams["eventi"] as $evento): ?>
+        <div class="evento list-container">
+          <h1>Evento: <?php echo $evento["nomeEvento"]?></h1>
+          <p>Data e ora inizio: <?php echo $evento["dataEOra"]?></p>
+          <p><?php echo $evento["nomeLuogo"].", ".$evento["indirizzo"];?></p>
+          <?php foreach ($_SESSION["sessUser"]["cart"][$evento["codEvento"]] as $tipoCosto => $numero): ?>
+            <div class="postiEvento">
+              <?php $arrTipoCosto = explode("/",$tipoCosto); ?>
+              <ul>
+                <li class="list-item">
+                  <h2>Tipologia: <?php echo $dbh->getSeatType($arrTipoCosto[0])["nomeTipologia"]; ?><p></h2><span class="right-aligned"><?php echo ($numero." x ".intval($arrTipoCosto[1])/100)."€ = ".(intval($arrTipoCosto[1]) / 100 * intval($numero))."€";?></span></p><p class="cartRemover" onclick='removeFromCart(<?php echo $evento["codEvento"].",".$arrTipoCosto[0].",".$arrTipoCosto[1].",".$numero;?>)'>Rimuovi voce</p>
+                </li>
+              </ul>
+            </div>
+          <?php endforeach; ?>
         </div>
-      <?php endforeach; ?>
+      <?php endforeach;?>
     </div>
-  <?php endforeach;?>
-  <p>Totale: <?php echo ($total/100)."€";?></p>
-  <a href = "php/checkout_process.php">Paga</a>
-<?php else:?>
-<p>Il tuo carrello è vuoto.</p>
-<?php endif;?>
+    <p class="right-aligned">Totale: <?php echo ($total/100)."€";?></p>
+    <button type="button" class="cartEmptier" onclick="emptyCart()">Svuota carrello</button><button class="pay" type="button"><a href = "php/checkout_process.php">Paga</a></button>
+  <?php else:?>
+  <p>Il tuo carrello è vuoto.</p>
+  <?php endif;?>
+</div>
