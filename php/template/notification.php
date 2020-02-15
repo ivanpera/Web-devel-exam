906652a1d5fc61f4c6df8d23fb74855a8c46752b
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-02-16 00:37:07 +0100

    template/notification: add main-content div

--- php/template/notification.php
+++ php/template/notification.php
@@ -1,22 +1,24 @@
-<?php if (count($templateParams["notifiche"]) > 0) :?>
-  <button type="button" onclick="readAll()">Segna tutti come letto</button>
-  <?php foreach($templateParams["notifiche"] as $notifica):?>
-    <a href="notification_details.php?codEvento=<?php echo $notifica["codEvento"]?>&codNotifica=<?php echo $notifica["codNotifica"]?>">
-      <article class="notification<?php echo $notifica["letta"] == false ? "unread" : "";?>" >
-        <h3><?php echo $notifica["titolo"];?></h3>
-        <p>Inviata alle <?php echo $notifica["dataEOraInvio"];?></p>
-        <p>
-          <?php 
-            if ($notifica["letta"] == false) {
-              echo "Da leggere.";
-            } else {
-              echo "Letta.";
-            }
-          ?>
-        </p>
+<div class="main-content">
+  <?php if (count($templateParams["notifiche"]) > 0) :?>
+    <button type="button" onclick="readAll()">Segna tutti come letto</button>
+    <?php foreach($templateParams["notifiche"] as $notifica):?>
+      <article class="notification<?php echo $notifica["letta"] == false ? "unread" : "";?> list-item" >
+        <a href="notification_details.php?codEvento=<?php echo $notifica["codEvento"]?>&codNotifica=<?php echo $notifica["codNotifica"]?>">
+          <h3><?php echo $notifica["titolo"];?></h3>
+          <p>Inviata alle <?php echo $notifica["dataEOraInvio"];?></p>
+          <p>
+            <?php 
+              if ($notifica["letta"] == false) {
+                echo "Da leggere.";
+              } else {
+                echo "Letta.";
+              }
+            ?>
+          </p>
+        </a>
       </article>
-    </a>
-  <?php endforeach;?>
-<?php else: ?>
-  <p>Non ci sono notifiche per te.</p>
-<?php endif;?>
+    <?php endforeach;?>
+  <?php else: ?>
+    <p>Non ci sono notifiche per te.</p>
+  <?php endif;?>
+</div>
