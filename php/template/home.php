8cba4a98edb1e48cd196f19d61b3ec13834ae47f
Alex Speranza <alexsperanza@live.it>
2020-02-16 15:11:06 +0100

    PHP: template/home.php: removes button inside <a>s and unifies <div> and <section> into a single <section>

--- php/template/home.php
+++ php/template/home.php
@@ -1,24 +1,18 @@
-<div class="headline_text">
+<section class="headline_text">
     <h1>
         Dacci i vaini, vendiamo biglietti
     </h1>
-</div>
-<section> <!-- Is section here useful? -->
-    <a href="events.php" class="composed_button_container">
-        <button type="button">
+    <a href="events.php" class="composed_button_container a-button">
             <img src="img/ticket.png" alt=""/>
             <section>
                 <h2> Cerca eventi </h2>
                 <p>Scopri cosa ha in serbo per te il mondo, dudo/etta </p>
             </section>
-        </button>
-    </a><a href="create_event.php" class="composed_button_container">
-        <button type="button">
+    </a><a href="create_event.php" class="composed_button_container a-button">
             <img src="img/ticket.png" alt=""/>
             <section>
                 <h2> Crea eventi </h2>
                 <p>Diventa più ganzo e fai i quattrini, crea ora now </p>
             </section>
-        </button>
     </a>
 </section>
