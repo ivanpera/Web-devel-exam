16f3dba948cb3a3c9d997ccc781254306705b7c8
Alex Speranza <alexsperanza@live.it>
2020-02-16 11:04:59 +0100

    Javascript: cart_handler: adds check in addToCart function; removes element specifier (now it has only the class selector) from the jquery in emptyCart

--- js/cart_handler.js
+++ js/cart_handler.js
@@ -1,16 +1,17 @@
 function addToCart(codEvento, codTipologia, costo) {
-  let numOfTickets = $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prev().val();
-
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if(this.readyState == 4 && this.status == 200) {
-      $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().html((xhttp.response == "OK" ? "Aggiunto al carrello!" : "Errore!"));
-      $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prop("disabled", true);
-    }
-  };
-  xhttp.open("POST", "php/ajax_response/cart_handler.php", true);
-  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("codEvento="+codEvento+"&codTipologia="+codTipologia+"&costo="+costo+"&num="+numOfTickets);
+  let numOfTickets = Number($('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prev().val());
+  if (numOfTickets <= Number($('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prev().attr("max"))) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if(this.readyState == 4 && this.status == 200) {
+        $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().html((xhttp.response == "OK" ? "Aggiunto al carrello!" : "Errore!"));
+        $('button[onclick="addToCart('+codEvento+','+codTipologia+','+ costo+')"]').first().prop("disabled", true);
+      }
+    };
+    xhttp.open("POST", "php/ajax_response/cart_handler.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("codEvento="+codEvento+"&codTipologia="+codTipologia+"&costo="+costo+"&num="+numOfTickets);
+  }
 }
 
 function removeFromCart(codEvento, codTipologia, costo, num) {
@@ -26,5 +27,5 @@ function removeFromCart(codEvento, codTipologia, costo, num) {
 }
 
 function emptyCart() {
-  $("button.cartRemover").each(function() {$(this).click();});
+  $(".cartRemover").each(function() {$(this).click();});
 }
