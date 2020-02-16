fa3995fb943b4c3eff540450da3fc8ec5836766d
Alex Speranza <alexsperanza@live.it>
2020-02-16 15:41:32 +0100

    CSS, PHP: fixes html to pass validation; changes style rules

--- php/template/login.php
+++ php/template/login.php
@@ -5,17 +5,15 @@
         }
     ?>
     <form action="php/login_process.php" method="POST">
-        <label for="email">Email: <br/><input type="email" id="email" name="email"></input></label><br/>
-        <label for="password">Password: <br/><input type="password" id="password" name="password" required></input></label><br/>
+        <label for="email">Email: <br/><input type="email" id="email" name="email"/></label><br/>
+        <label for="password">Password: <br/><input type="password" id="password" name="password" required/></label><br/>
         <input type="submit" id="btn_login" name="btn_login" value="Login"/>
     </form>
 
-    <section class="form-appendage">
-        <p>Non ancora registrato?</p>
-        <a href="signup.php">
-            <button type="button">
+    <div class="form-appendage">
+        <p>Non sei ancora registrato?</p>
+        <a href="signup.php" class="a-button">
                 Registrati
-            </button>
         </a>
-    </section>
+    </div>
 </div>
