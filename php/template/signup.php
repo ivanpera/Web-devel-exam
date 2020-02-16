22f262cd2578dcf7aeeb33f7633c9781052d6fa3
Alex Speranza <alexsperanza@live.it>
2020-02-16 15:52:48 +0100

    PHP: template/signup.php: changes html to pass validation

--- php/template/signup.php
+++ php/template/signup.php
@@ -6,12 +6,12 @@
 
     ?>
     <form action="php/signup_process.php" method="POST">
-        <label for="email">Email: <br/><input type="email" id="email" name="email" required></input></label><br/>
-        <label for="password">Password: <br/><input type="password" id="password" name="password" required></input></label><br/>
-        <label for="password_confirmation">Reinserisci la password: <br/><input type="password" id="password_confirmation" name="password_confirmation" required></input></label><br/>
-        <label for="name">Nome:<br/><input type="text" id="name" name="name" required></input></label><br/>
-        <label for="surname">Cognome: <br/><input type="text" id="surname" name="surname" required></input></label><br/>
-        <label for="birthdate">Data di nascita: <br/><input type="date" id="birthdate" name="birthdate" required></input></label><br/>
+        <label for="email">Email: <br/><input type="email" id="email" name="email" required/></label><br/>
+        <label for="password">Password: <br/><input type="password" id="password" name="password" required/></label><br/>
+        <label for="password_confirmation">Reinserisci la password: <br/><input type="password" id="password_confirmation" name="password_confirmation" required/></label><br/>
+        <label for="name">Nome:<br/><input type="text" id="name" name="name" required/></label><br/>
+        <label for="surname">Cognome: <br/><input type="text" id="surname" name="surname" required/></label><br/>
+        <label for="birthdate">Data di nascita: <br/><input type="date" id="birthdate" name="birthdate" required/></label><br/>
         <label for="gender">Genere: <select id="gender" name="gender" required>
                     <option value="" disabled selected hidden>Seleziona un'opzione...</option>
                     <option value="M">Maschio</option>
