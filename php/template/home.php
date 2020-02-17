6ed72291f33f3d574851aae918cdd6f31ecbce3f
Ivan Perazzini <ivan.perazzini@studio.unibo.it>
2020-02-17 15:35:51 +0100

    template/home: update icon names

--- php/template/home.php
+++ php/template/home.php
@@ -3,13 +3,13 @@
         Dacci i vaini, vendiamo biglietti
     </h1>
     <a href="events.php" class="composed_button_container a-button">
-            <img src="img/ticket.png" alt=""/>
+            <img src="img/find.png" alt=""/>
             <section>
                 <h2> Cerca eventi </h2>
                 <p>Scopri cosa ha in serbo per te il mondo, dudo/etta </p>
             </section>
     </a><a href="create_event.php" class="composed_button_container a-button">
-            <img src="img/ticket.png" alt=""/>
+            <img src="img/create.png" alt=""/>
             <section>
                 <h2> Crea eventi </h2>
                 <p>Diventa più ganzo e fai i quattrini, crea ora now </p>
