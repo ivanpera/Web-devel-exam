62ebe4cd2a89a3fa56a583613c2db6950a4168da
Alex Speranza <alexsperanza@live.it>
2020-02-17 14:33:16 +0100

    PHP: ajax/notification_num_getter: adds else branch

--- php/ajax_response/notification_num_getter.php
+++ php/ajax_response/notification_num_getter.php
@@ -2,6 +2,8 @@
   require_once("../../bootstrap.php");
   if(isset($_SESSION["sessUser"])) {
     echo $dbh->getUnreadNotificationNum($_SESSION["sessUser"]["email"]);
+  } else {
+    echo "0";
   }
 
 ?>
